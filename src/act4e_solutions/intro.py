05a9cf28853b55ce57ba6213538e0258ccc9eec2
amin-oj <aminojaghi93@gmail.com>
2024-04-15 18:19:45 +0330

    TestSimpleIntro

--- src/act4e_solutions/intro.py
+++ src/act4e_solutions/intro.py
@@ -3,4 +3,5 @@ import act4e_interfaces as I
 
 class SolSimpleIntro(I.SimpleIntro):
     def sum(self, a: int, b: int) -> int:
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        return a+b
